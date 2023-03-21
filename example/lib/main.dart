65afb1e7b79b32ad3bffa31797c7073efd184912
Mike <894659061@qq.com>
2023-03-21 09:45:33 +0800

    暂存

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:flutterfmod/flutterfmod.dart';
 import 'package:permission_handler/permission_handler.dart';
 
--- test/flutterfmod_test.dart
+++ test/flutterfmod_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutterfmod/flutterfmod.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutterfmod');
@@ -16,8 +15,4 @@ void main() {
   tearDown(() {
     channel.setMockMethodCallHandler(null);
   });
-
-  // test('getPlatformVersion', () async {
-  //   expect(await Flutterfmod.platformVersion, '42');
-  // });
 }
